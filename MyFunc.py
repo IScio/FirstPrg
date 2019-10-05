cf173956c5130fa0446aeefe3706e14681cd8907
IScio <innocent.scio@gmail.com>
2019-10-05 16:39:44 +0300

    My First Program

---
+++ MyFunc.py
@@ -0,0 +1,15 @@
+def sum():
+    a=int(input("Enter Value A: "))
+    b=int(input("Enter Value B: "))
+
+    total=a+b
+    if total >=50:
+      return "Above 50"
+    else:
+      return total
+
+print(sum())
+
+def circum():
+  #Circumference of a circle
+  
